6d348bdee700da2e14ce5dd7f12f9945ac5660e8
Scott Koranda <skoranda@gmail.com>
2021-10-11 13:03:49 -0500

    Add code to query for petition details and set session
    A temporary commit to demonstrate how to add code to
    query using the petition ID to find the current petition
    being used to enroll the user, and how to add the onFinish
    redirect URL object (array) to the Session so it can be
    retrieved later.

--- Controller/IgwnEffortEnrollerCoPetitionsController.php
+++ Controller/IgwnEffortEnrollerCoPetitionsController.php
@@ -10,12 +10,22 @@ class IgwnEffortEnrollerCoPetitionsController extends CoPetitionsController {
   protected function execute_plugin_finalize($id, $onFinish) {
     // IGWN effort logic goes here.
 
+    $this->log("IgwnEffort enroller finalize is called with petition $id and " . print_r($onFinish, true));
+
+    $args = array();
+    $args['conditions']['CoPetition.id'] = $id;
+    $args['contain'] = false;
+
+    $coPetition = $this->CoPetition->find('first', $args);
+
+    $this->log("Found petition " . print_r($coPetition, true));
 
 
-    $this->log("IgwnEffort enroller finalize is called");
+    $this->Session->write('Igwn.plugin.effort.enroller.onFinish', $onFinish);
 
+    $foo = $this->Session->read('Igwn.plugin.effort.enroller.onFinish');
 
-
+    $this->log("Foo is " . print_r($foo, true));
 
 
     // Redirect now that we are done.
