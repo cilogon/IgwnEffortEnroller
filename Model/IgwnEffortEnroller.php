112b44fc59566bed196a2c827c2e7ebfdd61757e
Scott Koranda <skoranda@gmail.com>
2021-11-12 07:06:04 -0600

    Update for COmanage Registry 4.0.0
    Add necessary details so that the plugin can be
    instantiated and added to enrollment flows as a wedge
    for COmanage Registry version 4.0.0.

--- Model/IgwnEffortEnroller.php
+++ Model/IgwnEffortEnroller.php
@@ -4,6 +4,24 @@ class IgwnEffortEnroller extends AppModel {
   // Required by COmanage Plugins
   public $cmPluginType = "enroller";
 
+  // Document foreign keys
+  public $cmPluginHasMany = array();
+
+  // Add behaviors
+  public $actsAs = array('Containable', 'Changelog' => array('priority' => 5));
+
+  // Association rules from this model to other models
+  public $belongsTo = array("CoEnrollmentFlowWedge");
+
+  // Validation rules for table elements
+  public $validate = array(
+    'co_enrollment_flow_wedge_id' => array(
+      'rule' => 'numeric',
+      'required' => true,
+      'allowEmpty' => false
+    )
+  );
+
   public function cmPluginMenus() {
     return array();
   }
